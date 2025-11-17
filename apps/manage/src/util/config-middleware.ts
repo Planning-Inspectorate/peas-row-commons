3a8fa15978024797d060d18587fb7d87e4fac63e
Oscar Gay <oscar.gay@solirius.com>
2025-11-17 14:05:37 +0000

    chore: updates style file

--- apps/manage/src/util/config-middleware.ts
+++ apps/manage/src/util/config-middleware.ts
@@ -6,7 +6,7 @@ import type { Handler } from 'express';
 export function addLocalsConfiguration(): Handler {
 	return (req, res, next) => {
 		res.locals.config = {
-			styleFile: 'style-2c953252.css',
+			styleFile: 'style-00a9e03e.css',
 			headerTitle: 'App 2 Service'
 		};
 		next();
