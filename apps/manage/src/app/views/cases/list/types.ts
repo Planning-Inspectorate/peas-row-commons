c6cc85420a3e6bec29cc0054de293766c024bc94
Oscar Gay <oscar.gay@solirius.com>
2025-11-18 14:38:48 +0000

    feat(manage): adds typing so we can create a view model with a sortable date

---
+++ apps/manage/src/app/views/cases/list/types.ts
@@ -0,0 +1,22 @@
+import { Prisma } from '@pins/peas-row-commons-database/src/client/client.js';
+
+export const caseListSelect = {
+	id: true,
+	name: true,
+	reference: true,
+	receivedDate: true,
+	Type: {
+		select: { displayName: true }
+	}
+} satisfies Prisma.CaseSelect;
+
+export type CaseListFields = Prisma.CaseGetPayload<{ select: typeof caseListSelect }>;
+
+export interface CaseListViewModel {
+	id: string;
+	reference: string;
+	receivedDate: Date;
+	name: string;
+	Type: { displayName: string | null };
+	receivedDateSortable: number;
+}
