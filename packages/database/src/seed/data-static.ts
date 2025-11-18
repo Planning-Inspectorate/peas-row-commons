89af7010f638723d5fca44e7e11855e10cd93d6e
Oscar Gay <oscar.gay@solirius.com>
2025-11-18 09:58:46 +0000

    feat(database): creates seed data for prod

--- packages/database/src/seed/data-static.ts
+++ packages/database/src/seed/data-static.ts
@@ -1,8 +1,21 @@
 import type { PrismaClient } from '@pins/peas-row-commons-database/src/client/client.ts';
-// TODO: import data from ./static-data/index.ts
+import { CASEWORK_AREAS, CASE_TYPES, CASE_SUBTYPES } from './static_data/index.ts';
+
+// TODO: under no circumstances should you commit this ghastly typing
+async function upsertReferenceData({ delegate, input }: any) {
+	return delegate.upsert({
+		create: input,
+		update: input,
+		where: { id: input.id }
+	});
+}
 
 export async function seedStaticData(dbClient: PrismaClient) {
-	// TODO: add static seed data
-	await dbClient.$queryRaw`SELECT 1`;
+	await Promise.all(CASEWORK_AREAS.map((input) => upsertReferenceData({ delegate: dbClient.caseworkArea, input })));
+
+	await Promise.all(CASE_TYPES.map((input) => upsertReferenceData({ delegate: dbClient.caseType, input })));
+
+	await Promise.all(CASE_SUBTYPES.map((input) => upsertReferenceData({ delegate: dbClient.caseSubType, input })));
+
 	console.log('static data seed complete');
 }
