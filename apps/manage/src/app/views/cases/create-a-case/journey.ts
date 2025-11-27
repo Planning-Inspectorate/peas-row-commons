16217051c5e1f851a34a22960fdd9fa9cb8de3f6
Venki <Venki.Sorag.47@planninginspectorate.gov.uk>
2025-11-27 13:29:39 +0000

    feat: added a conditinal rendering for questions

--- apps/manage/src/app/views/cases/create-a-case/journey.ts
+++ apps/manage/src/app/views/cases/create-a-case/journey.ts
@@ -2,6 +2,8 @@ import { Section } from '@planning-inspectorate/dynamic-forms/src/section.js';
 import { Journey } from '@planning-inspectorate/dynamic-forms/src/journey/journey.js';
 import type { Handler, Request } from 'express';
 import { questionHasAnswer } from '@planning-inspectorate/dynamic-forms/src/components/utils/question-has-answer.js';
+import { CASEWORK_AREAS_ID } from '@pins/peas-row-commons-database/src/seed/static_data/ids/casework-areas.ts';
+import { CASE_TYPES_ID } from '@pins/peas-row-commons-database/src/seed/static_data/ids/types.ts';
 
 export const JOURNEY_ID = 'create-a-case';
 
@@ -13,13 +15,16 @@ export function createJourney(documentTypeId: string, questions: any, response: 
 	return new Journey({
 		journeyId: documentTypeId,
 		sections: [
-			  new Section('', 'questions')
-			 .addQuestion(questions.caseworkArea) 
-			 .addQuestion(questions.planningEnvironmentApplications)
-			 .withCondition((response:any) => questionHasAnswer(response, questions.caseworkArea, 'planning-environment-applications'))
-		     .addQuestion(questions.drought)
-			 .withCondition((response:any) => questionHasAnswer(response, questions.planningEnvironmentApplications, 'drought'))
-			 .addQuestion(questions.rightsOfWayAndCommonLand)
+
+			 	new Section('', 'questions')
+				.addQuestion(questions.caseworkArea)
+				.addQuestion(questions.planningEnvironmentApplications)
+				.withCondition((response: any) => questionHasAnswer(response,questions.caseworkArea,CASEWORK_AREAS_ID.PLANNING_ENVIRONMENTAL_APPLICATIONS))
+				.addQuestion(questions.drought)
+				.withCondition((response: any) => questionHasAnswer(response,questions.planningEnvironmentApplications,CASE_TYPES_ID.DROUGHT))
+				.addQuestion(questions.rightsOfWayAndCommonLand)
+				.withCondition((response: any) => questionHasAnswer(response,questions.caseworkArea, CASEWORK_AREAS_ID.RIGHTS_OF_WAY_COMMON_LAND)
+				)
 		
 		],
 		taskListUrl: 'check-your-answers',
