c069361ff7b7f397023004f6133fcfae1a3eefc5
Oscar Gay <oscar.gay@solirius.com>
2025-12-01 16:41:16 +0000

    feat(manage): updates the view model and get functions for the view to account for Dates

--- apps/manage/src/app/views/cases/view/view-model.ts
+++ apps/manage/src/app/views/cases/view/view-model.ts
@@ -2,10 +2,21 @@ import type { CaseListFields } from './types.ts';
 import { formatInTimeZone } from 'date-fns-tz';
 
 export function caseToViewModel(caseRow: CaseListFields) {
-	const viewModel = {
-		...caseRow,
-		receivedDate: formatInTimeZone(caseRow.receivedDate, 'Europe/London', 'dd MMM yyyy'),
+	const { Dates, ...caseData } = caseRow;
+
+	// We need to create a soft copy here because we want
+	// to delete the id so that we don't overwrite the Case
+	// id when we spread.
+	const safeDates = { ...Dates };
+
+	if (safeDates.id) {
+		delete safeDates.id;
+	}
+
+	return {
+		...caseData,
+		...safeDates,
+		receivedDateDisplay: formatInTimeZone(caseRow.receivedDate, 'Europe/London', 'dd MMM yyyy'),
 		receivedDateSortable: new Date(caseRow.receivedDate)?.getTime()
 	};
-	return viewModel;
 }
