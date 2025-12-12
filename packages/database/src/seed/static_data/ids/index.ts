8daf337bb13dad519aeb25f627ed2526f34e5c09
Oscar Gay <oscar.gay@solirius.com>
2025-12-12 16:10:05 +0000

    refactor: moves invoice statuses into the static data dir for consistency

--- packages/database/src/seed/static_data/ids/index.ts
+++ packages/database/src/seed/static_data/ids/index.ts
@@ -2,3 +2,4 @@ export { CASEWORK_AREAS_ID } from './casework-areas.ts';
 export { CASE_TYPES_ID } from './types.ts';
 export { CASE_SUBTYPES_ID } from './subtypes.ts';
 export { PROCEDURES_ID } from './procedures.ts';
+export { INVOICE_STATUSES_ID } from './invoices.ts';
