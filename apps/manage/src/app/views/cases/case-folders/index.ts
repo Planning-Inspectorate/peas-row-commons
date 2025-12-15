d42d103f78f57cc89d7b0edd68b4ad129cd32edc
Oscar Gay <oscar.gay@solirius.com>
2025-12-15 12:49:26 +0000

    feat(manage): creates the individual folder page

--- apps/manage/src/app/views/cases/case-folders/index.ts
+++ apps/manage/src/app/views/cases/case-folders/index.ts
@@ -3,13 +3,18 @@ import { asyncHandler } from '@pins/peas-row-commons-lib/util/async-handler.ts';
 import { validateIdFormat } from '../view/controller.ts';
 import { buildViewCaseFolders } from './controller.ts';
 import type { ManageService } from '#service';
+import { buildViewCaseFolder } from './case-folder/controller.ts';
 
 export function createRoutes(service: ManageService) {
 	const router = createRouter({ mergeParams: true });
-	const viewCaseDetails = buildViewCaseFolders(service);
+	const viewCaseFolders = buildViewCaseFolders(service);
+	const viewCaseFolder = buildViewCaseFolder(service);
 
 	// Gets "all folders" page
-	router.get('/', validateIdFormat, asyncHandler(viewCaseDetails));
+	router.get('/', validateIdFormat, asyncHandler(viewCaseFolders));
+
+	// Gets "individual folder" page
+	router.get('/:folderId/:folderName', validateIdFormat, asyncHandler(viewCaseFolder));
 
 	return router;
 }
