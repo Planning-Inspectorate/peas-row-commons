3f60dc0358478b8e0c594ad543cac9c83f07f5c3
Oscar Gay <oscar.gay@solirius.com>
2025-12-17 16:36:31 +0000

    feat(manage): adds sortable table for displaying documents

--- apps/manage/src/app/views/cases/case-folders/case-folder/controller.ts
+++ apps/manage/src/app/views/cases/case-folders/case-folder/controller.ts
@@ -3,6 +3,7 @@ import { notFoundHandler } from '@pins/peas-row-commons-lib/middleware/errors.ts
 import type { AsyncRequestHandler } from '@pins/peas-row-commons-lib/util/async-handler.ts';
 import { wrapPrismaError } from '@pins/peas-row-commons-lib/util/database.ts';
 import { createFoldersViewModel } from '../view-model.ts';
+import { createDocumentsViewModel } from './view-model.ts';
 
 export function buildViewCaseFolder(service: ManageService): AsyncRequestHandler {
 	const { db, logger } = service;
@@ -18,9 +19,9 @@ export function buildViewCaseFolder(service: ManageService): AsyncRequestHandler
 			throw new Error('folderId param required');
 		}
 
-		let caseRow, currentFolder, subFolders;
+		let caseRow, currentFolder, subFolders, documents;
 		try {
-			[caseRow, currentFolder, subFolders] = await Promise.all([
+			[caseRow, currentFolder, subFolders, documents] = await Promise.all([
 				db.case.findUnique({
 					select: {
 						reference: true,
@@ -33,6 +34,9 @@ export function buildViewCaseFolder(service: ManageService): AsyncRequestHandler
 				}),
 				db.folder.findMany({
 					where: { caseId: id, parentFolderId: folderId } // Get children of parent
+				}),
+				db.document.findMany({
+					where: { caseId: id, folderId }
 				})
 			]);
 		} catch (error: any) {
@@ -50,6 +54,8 @@ export function buildViewCaseFolder(service: ManageService): AsyncRequestHandler
 
 		const subFoldersViewModel = subFolders ? createFoldersViewModel(subFolders) : [];
 
+		const documentsViewModel = documents ? createDocumentsViewModel(documents) : [];
+
 		const baseFoldersUrl = `/cases/${id}/case-folders`;
 
 		return res.render('views/cases/case-folders/case-folder/view.njk', {
@@ -59,7 +65,8 @@ export function buildViewCaseFolder(service: ManageService): AsyncRequestHandler
 			backLinkUrl: baseFoldersUrl,
 			baseFoldersUrl, // Used for creating the url of the sub-folders
 			subFolders: subFoldersViewModel,
-			currentUrl: req.originalUrl
+			currentUrl: req.originalUrl,
+			documents: documentsViewModel
 		});
 	};
 }
