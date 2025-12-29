3217b21d4695632fa868e5186327e3b50d87b7f5
Oscar Gay <oscar.gay@solirius.com>
2025-12-29 12:05:10 +0000

    feat(database): updates static and dev seed

--- packages/database/src/seed/static_data/ids/index.ts
+++ packages/database/src/seed/static_data/ids/index.ts
@@ -3,3 +3,6 @@ export { CASE_TYPES_ID } from './types.ts';
 export { CASE_SUBTYPES_ID } from './subtypes.ts';
 export { PROCEDURES_ID } from './procedures.ts';
 export { INVOICE_STATUSES_ID } from './invoices.ts';
+export { PRIORITY_ID } from './priority.ts';
+export { CASE_STATUS_ID } from './status.ts';
+export { ADVERTISED_MODIFICATION_ID } from './advertised-modification.ts';
