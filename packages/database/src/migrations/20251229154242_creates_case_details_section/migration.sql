08f1ef1434ec9a9d4922228baf9a8ec11187e0b8
Oscar Gay <oscar.gay@solirius.com>
2025-12-29 15:51:17 +0000

    feat(manage): adds the changes needed for create a case

--- packages/database/src/migrations/20251229154242_creates_case_details_section/migration.sql
+++ packages/database/src/migrations/20251229154242_creates_case_details_section/migration.sql
@@ -47,8 +47,8 @@ CREATE TABLE [dbo].[CasePriority] (
 CREATE TABLE [dbo].[Applicant] (
     [id] UNIQUEIDENTIFIER NOT NULL CONSTRAINT [Applicant_id_df] DEFAULT newid(),
     [name] NVARCHAR(1000) NOT NULL,
-    [email] NVARCHAR(1000) NOT NULL,
-    [telephoneNumber] NVARCHAR(1000) NOT NULL,
+    [email] NVARCHAR(1000),
+    [telephoneNumber] NVARCHAR(1000),
     CONSTRAINT [Applicant_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
@@ -56,8 +56,8 @@ CREATE TABLE [dbo].[Applicant] (
 CREATE TABLE [dbo].[Authority] (
     [id] UNIQUEIDENTIFIER NOT NULL CONSTRAINT [Authority_id_df] DEFAULT newid(),
     [name] NVARCHAR(1000) NOT NULL,
-    [email] NVARCHAR(1000) NOT NULL,
-    [telephoneNumber] NVARCHAR(1000) NOT NULL,
+    [email] NVARCHAR(1000),
+    [telephoneNumber] NVARCHAR(1000),
     CONSTRAINT [Authority_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
