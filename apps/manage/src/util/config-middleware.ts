17ae3a00434ab7500294d0b76073beb0c8d84935
Oscar Gay <oscar.gay@solirius.com>
2025-12-30 12:11:40 +0000

    feat(manage): adds MPESC header

--- apps/manage/src/util/config-middleware.ts
+++ apps/manage/src/util/config-middleware.ts
@@ -7,7 +7,7 @@ export function addLocalsConfiguration(): Handler {
 	return (req, res, next) => {
 		res.locals.config = {
 			styleFile: 'style-f0a89ff5.css',
-			headerTitle: 'Service name placeholder'
+			headerTitle: 'Manage planning, environmental and specialist casework'
 		};
 		next();
 	};
