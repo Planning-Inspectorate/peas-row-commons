4d4ba76c582dc6488683157df78806dadb881aa9
Oscar Gay <oscar.gay@solirius.com>
2026-01-10 13:52:52 +0000

    feat(lib): adds custom time component

--- packages/lib/forms/custom-components/index.ts
+++ packages/lib/forms/custom-components/index.ts
@@ -1,12 +1,15 @@
 import ConditionalOptionsQuestion from './conditional-text-input/question.ts';
 import FencingPermanentQuestion from './fencing-permanent/question.ts';
+import OptionalTimeDateTimeInput from './optional-time-date-time-input/question.ts';
 
 export const CUSTOM_COMPONENTS = Object.freeze({
 	CONDITIONAL_TEXT_OPTIONS: 'conditional-text-options',
-	FENCING_PERMANENT: 'fencing-permanent'
+	FENCING_PERMANENT: 'fencing-permanent',
+	OPTIONAL_TIME_DATE_TIME: 'optional-time-date-time'
 });
 
 export const CUSTOM_COMPONENT_CLASSES = Object.freeze({
 	[CUSTOM_COMPONENTS.CONDITIONAL_TEXT_OPTIONS]: ConditionalOptionsQuestion,
-	[CUSTOM_COMPONENTS.FENCING_PERMANENT]: FencingPermanentQuestion
+	[CUSTOM_COMPONENTS.FENCING_PERMANENT]: FencingPermanentQuestion,
+	[CUSTOM_COMPONENTS.OPTIONAL_TIME_DATE_TIME]: OptionalTimeDateTimeInput
 });
