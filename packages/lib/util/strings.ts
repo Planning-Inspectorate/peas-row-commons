b29cc9b1fd241ccbe4d6c43254c51a947f2d4621
Oscar Gay <oscar.gay@solirius.com>
2026-01-20 10:38:48 +0000

    fix: uses kebab case for folders in url

--- packages/lib/util/strings.ts
+++ packages/lib/util/strings.ts
@@ -9,7 +9,8 @@ export function stringToKebab(string: string): string {
 	if (!string) return '';
 	return string
 		.replace(/([a-z])([A-Z])/g, '$1-$2')
-		.replace(/[\s_]+/g, '-')
+		.replace(/[\W_]+/g, '-')
+		.replace(/^-+|-+$/g, '')
 		.toLowerCase();
 }
 
