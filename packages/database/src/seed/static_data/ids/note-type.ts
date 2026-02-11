a68e03a8affaf65f527a32007f950a6be5582681
Abbas Merali <abbas.merali@solirius.com>
2026-02-11 11:19:16 +0000

    fix(database): address static data exports for note types

--- packages/database/src/seed/static_data/ids/note-type.ts
+++ packages/database/src/seed/static_data/ids/note-type.ts
@@ -1,7 +1,5 @@
-export const NOTE_TYPE_ID = {
+export const NOTE_TYPE_ID = Object.freeze({
 	CASE_NOTE: 'case-note',
 	STICKY_NOTE: 'sticky-note',
 	IMPORTANT_INFO: 'important-info'
-} as const;
-
-export const { CASE_NOTE, STICKY_NOTE, IMPORTANT_INFO } = NOTE_TYPE_ID;
+});
