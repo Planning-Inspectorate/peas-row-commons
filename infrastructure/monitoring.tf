b93be23e22ad42df6223badfeb69894c4c1da38f
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2026-02-16 12:07:34 +0000

    feat(infrastructure): adding log query alert for log analytics workspace

--- infrastructure/monitoring.tf
+++ infrastructure/monitoring.tf
@@ -79,6 +79,40 @@ resource "azurerm_monitor_metric_alert" "manage_availability" {
   }
 }
 
+# Log cap alert using scheduled query rules
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "log_cap" {
+  count = var.environment == "prod" ? 1 : 0
+
+  name         = "Log cap Alert"
+  display_name = "log Daily data limit reached"
+  description  = "Triggered when the log Data cap is reached."
+
+  location            = module.primary_region.location
+  resource_group_name = azurerm_resource_group.primary.name
+  scopes              = [azurerm_log_analytics_workspace.main.id]
+
+  enabled                 = true
+  auto_mitigation_enabled = false
+
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+
+  criteria {
+    query                   = <<-QUERY
+      _LogOperation
+      | where Category =~ "Ingestion" | where Detail contains "OverQuota"
+      QUERY
+    time_aggregation_method = "Count"
+    threshold               = 0
+    operator                = "GreaterThan"
+  }
+
+  severity = 2
+  action {
+    action_groups = [local.action_group_ids.tech]
+  }
+}
+
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   #checkov:skip=CKV_AZURE_41: expiration not valid
 
