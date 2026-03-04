0bc6f5d65d3af31bbfdaddda36f535e4702216ba
Oscar Gay <oscar.gay@solirius.com>
2026-03-04 14:50:53 +0000

    feat: adds assigned to me page

--- apps/manage/src/app/views/cases/index.ts
+++ apps/manage/src/app/views/cases/index.ts
@@ -5,15 +5,25 @@ import type { ManageService } from '#service';
 import type { IRouter } from 'express';
 import { createNewCaseRoutes } from './create-a-case/index.ts';
 import { createRoutes as createCaseRoutes } from './view/index.ts';
+import { createRoutes as createPersonalListRoutes } from './personal-list/index.ts';
 
 export function createRoutes(service: ManageService): IRouter {
 	const router = createRouter({ mergeParams: true });
 	const listCases = buildListCases(service);
 	const createACaseRoutes = createNewCaseRoutes(service);
 	const caseRoutes = createCaseRoutes(service);
+	const personalListRoutes = createPersonalListRoutes(service);
 
+	// Main list page / landing page
 	router.get('/', asyncHandler(listCases));
+
+	// Page / flow for creating a new case
 	router.use('/create-a-case', asyncHandler(createACaseRoutes));
+
+	// Cases that a user is assigned to either as case officer or inspector
+	router.use('/personal-list', personalListRoutes);
+
+	// Specific case view
 	router.use('/:id', caseRoutes);
 	return router;
 }
