cded1804ec7366a43b2fc6c4f5f1116f707b6a3a
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-03-06 14:28:02 +0000

    feat: creating private endpoint for key vault

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -48,6 +48,13 @@ data "azurerm_private_dns_zone" "storage" {
   provider = azurerm.tooling
 }
 
+data "azurerm_private_dns_zone" "keyvault" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = var.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
+
 data "azurerm_virtual_network" "tooling" {
   name                = var.tooling_config.network_name
   resource_group_name = var.tooling_config.network_rg
