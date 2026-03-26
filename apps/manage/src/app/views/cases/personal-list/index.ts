e3ecbeb945ca55bf3381e895e9abc3fea2bf8f54
Oscar Gay <oscar.gay@solirius.com>
2026-03-26 12:22:20 +0000

    feat(manage): adds assigned to someone page

--- apps/manage/src/app/views/cases/personal-list/index.ts
+++ apps/manage/src/app/views/cases/personal-list/index.ts
@@ -1,17 +1,31 @@
 import { Router as createRouter } from 'express';
 import type { ManageService } from '#service';
 import type { IRouter } from 'express';
-import { buildViewPersonalList } from './controller.ts';
+import { buildFindSelectedUser, buildSelectUserView, buildViewPersonalList } from './controller.ts';
 
 export function createRoutes(service: ManageService): IRouter {
 	const router = createRouter({ mergeParams: true });
 
-	const personalListView = buildViewPersonalList(service);
+	const [personalListView, selectUserView, findSelectedUser] = createMiddlewares(service);
 
 	router
 		.route('/')
 		// Gets the "assigned to me" page
 		.get(personalListView);
 
+	router
+		.route('/select-user')
+		// Page for picking a new user to see
+		.get(selectUserView)
+		// Viewing that new user once selected
+		.post(findSelectedUser);
+
 	return router;
+}
+
+/**
+ * Creates the middleware functions needed for the endpoints
+ */
+function createMiddlewares(service: ManageService) {
+	return [buildViewPersonalList(service), buildSelectUserView(service), buildFindSelectedUser(service)];
 }
