f24cf55d23aaa2c65e255cc577b0d4c31bcf0ace
Paymon Sattarzadeh <paymonsattar@gmail.com>
2026-04-09 16:49:46 +0100

    feat: wip4

---
+++ packages/database/src/migrations/20260409154556_add_index_to_contacts/migration.sql
@@ -0,0 +1,28 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateIndex
+CREATE NONCLUSTERED INDEX [Contact_caseId_contactTypeId_idx] ON [dbo].[Contact]([caseId], [contactTypeId]);
+
+-- CreateIndex
+CREATE NONCLUSTERED INDEX [Contact_contactTypeId_firstName_idx] ON [dbo].[Contact]([contactTypeId], [firstName]);
+
+-- CreateIndex
+CREATE NONCLUSTERED INDEX [Contact_contactTypeId_lastName_idx] ON [dbo].[Contact]([contactTypeId], [lastName]);
+
+-- CreateIndex
+CREATE NONCLUSTERED INDEX [Contact_contactTypeId_orgName_idx] ON [dbo].[Contact]([contactTypeId], [orgName]);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
