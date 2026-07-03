b83a2b6cd7173b64997b70e94f2a551592d9f455
Alex Moulin <alex.moulin@planninginspectorate.gov.uk>
2026-07-03 13:38:28 +0100

    chore(infra): remove GOV_NOTIFY_API_KEY and related secret from configuration

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -9,7 +9,6 @@ locals {
 
   secrets = [
     "peas-client-secret",
-    "peas-gov-notify-api-key",
     "microsoft-provider-authentication-secret",
     "authorities-change-request-email"
   ]
@@ -19,7 +18,7 @@ locals {
       for k, v in azurerm_key_vault_secret.manual_secrets : k => "@Microsoft.KeyVault(SecretUri=${v.versionless_id})"
     },
     {
-      "app-insights-connection-string" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.app_insights_connection_string.versionless_id})",
+      "app-insights-connection-string" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.app_insights_connection_string.versionless_id})"
       "redis-connection-string"        = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.redis_web_connection_string.versionless_id})"
       "session-secret-web"             = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.web_session_secret.versionless_id})"
       "sql-app-connection-string"      = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.sql_app_connection_string.versionless_id})"
@@ -31,8 +30,8 @@ locals {
   action_group_ids = {
     tech            = azurerm_monitor_action_group.peas_tech.id
     service_manager = azurerm_monitor_action_group.peas_service_manager.id
-    iap             = data.azurerm_monitor_action_group.common["iap"].id,
-    its             = data.azurerm_monitor_action_group.common["its"].id,
+    iap             = data.azurerm_monitor_action_group.common["iap"].id
+    its             = data.azurerm_monitor_action_group.common["its"].id
     info_sec        = data.azurerm_monitor_action_group.common["info_sec"].id
   }
 
