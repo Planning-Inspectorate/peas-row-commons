1e80d56214b7065fdad99e3cf9b230c6e7fb68cc
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-08-06 13:49:52 +0100

    chore: add linting for type only imports

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default defineConfig([
 	eslintConfigPrettier,
 	{
 		rules: {
+			'@typescript-eslint/consistent-type-imports': 'error',
 			'@typescript-eslint/no-explicit-any': 'off'
 		}
 	},
