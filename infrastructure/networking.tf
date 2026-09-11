d16c6b9c5d7fc26784ad2c47c82eb92491b4c593
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-09-11 09:52:40 +0100

    chore(infrastructure): updating TF code for azurerm v5

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -61,46 +61,41 @@ resource "azurerm_virtual_network_peering" "tooling_to_peas" {
 ## DNS Zones for Azure Services
 ## Private DNS Zones exist in the tooling subscription and are shared here
 resource "azurerm_private_dns_zone_virtual_network_link" "dns_database" {
-  name                  = "${local.org}-vnetlink-db-${local.resource_suffix}"
-  resource_group_name   = var.tooling_config.network_rg
-  private_dns_zone_name = data.azurerm_private_dns_zone.database.name
-  virtual_network_id    = azurerm_virtual_network.main.id
+  name                = "${local.org}-vnetlink-db-${local.resource_suffix}"
+  private_dns_zone_id = data.azurerm_private_dns_zone.database.id
+  virtual_network_id  = azurerm_virtual_network.main.id
 
   provider = azurerm.tooling
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "app_service" {
-  name                  = "${local.org}-vnetlink-app-service-${local.resource_suffix}"
-  resource_group_name   = var.tooling_config.network_rg
-  private_dns_zone_name = data.azurerm_private_dns_zone.app_service.name
-  virtual_network_id    = azurerm_virtual_network.main.id
+  name                = "${local.org}-vnetlink-app-service-${local.resource_suffix}"
+  private_dns_zone_id = data.azurerm_private_dns_zone.app_service.id
+  virtual_network_id  = azurerm_virtual_network.main.id
 
   provider = azurerm.tooling
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "redis_cache" {
-  name                  = "${local.org}-vnetlink-redis-cache-${local.resource_suffix}"
-  resource_group_name   = var.tooling_config.network_rg
-  private_dns_zone_name = data.azurerm_private_dns_zone.redis_cache.name
-  virtual_network_id    = azurerm_virtual_network.main.id
+  name                = "${local.org}-vnetlink-redis-cache-${local.resource_suffix}"
+  private_dns_zone_id = data.azurerm_private_dns_zone.redis_cache.id
+  virtual_network_id  = azurerm_virtual_network.main.id
 
   provider = azurerm.tooling
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "doc_storage" {
-  name                  = "${local.org}-vnetlink-doc-storage-${local.resource_suffix}"
-  resource_group_name   = var.tooling_config.network_rg
-  private_dns_zone_name = data.azurerm_private_dns_zone.storage.name
-  virtual_network_id    = azurerm_virtual_network.main.id
+  name                = "${local.org}-vnetlink-doc-storage-${local.resource_suffix}"
+  private_dns_zone_id = data.azurerm_private_dns_zone.storage.id
+  virtual_network_id  = azurerm_virtual_network.main.id
 
   provider = azurerm.tooling
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "keyvault" {
-  name                  = "${local.org}-vnetlink-keyvault-${local.resource_suffix}"
-  resource_group_name   = var.tooling_config.network_rg
-  private_dns_zone_name = data.azurerm_private_dns_zone.keyvault.name
-  virtual_network_id    = azurerm_virtual_network.main.id
+  name                = "${local.org}-vnetlink-keyvault-${local.resource_suffix}"
+  private_dns_zone_id = data.azurerm_private_dns_zone.keyvault.id
+  virtual_network_id  = azurerm_virtual_network.main.id
 
   tags = local.tags
 
